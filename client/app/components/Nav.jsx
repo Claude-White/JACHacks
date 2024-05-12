887ffcdc69c990f2bd36109f7226897caf3cc827
Matteo-Falasconi <119690671+Matteo-Falasconi@users.noreply.github.com>
2024-05-12 10:47:37 -0400

    fixed nav

--- client/app/components/Nav.jsx
+++ client/app/components/Nav.jsx
@@ -1,22 +1,40 @@
-
 import Link from "next/link";
 
 const Nav = () => {
-
-
   return (
     <>
       <li>
-      <Link href="/">Home</Link>
-      </li> 
+        <Link href="/">Home</Link>
+      </li>
+      <h3>-- Computer Science --</h3>
       <li>
-      <Link href="../ChatRoom?class=Computer-Science">Computer Science</Link>
+        <Link href="/ChatRoom?class=Computer-Science&dif=beginner">Beginner</Link>
       </li>
       <li>
-      <Link href="../ChatRoom?class=Geography">Geography</Link>
+        <Link href="/ChatRoom?class=Computer-Science&dif=intermediate">Intermediate</Link>
       </li>
       <li>
-      <Link href="../ChatRoom?class=History">History</Link>
+        <Link href="/ChatRoom?class=Computer-Science&dif=advanced">Advanced</Link>
+      </li>
+      <h3>-- Geography --</h3>
+      <li>
+        <Link href="/ChatRoom?class=Geography&dif=beginner">Beginner</Link>
+      </li>
+      <li>
+        <Link href="/ChatRoom?class=Geography&dif=intermediate">Intermediate</Link>
+      </li>
+      <li>
+        <Link href="/ChatRoom?class=Geography&dif=advanced">Advanced</Link>
+      </li>
+      <h3>-- History --</h3>
+      <li>
+        <Link href="/ChatRoom?class=History&dif=beginner">Beginner</Link>
+      </li>
+      <li>
+        <Link href="/ChatRoom?class=History&dif=intermediate">Intermediate</Link>
+      </li>
+      <li>
+        <Link href="/ChatRoom?class=History&dif=advanced">Advanced</Link>
       </li>
     </>
   );
